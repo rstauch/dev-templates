2407cfe5c8f43a2a84b8ab02bced9ba6f3629d59
Stauch, R. (Robert) <robert.stauch@fluxdev.de>
2023-05-07 11:26:30 +0000

    java-11

---
+++ java/11/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A flake for GraalVM, JDK, Maven and Gradle";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: let
+    java_version = "11";
+  in
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      jdk = pkgs."jdk${java_version}";
+      gradle = pkgs.gradle.override {java = jdk;};
+      graalvm = pkgs."graalvm${java_version}-ce";
+      mvn = pkgs.maven;
+      javaVersion = java_version;
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit javaVersion;
+        buildInputs = [graalvm jdk gradle mvn];
+
+        shellHook = ''
+          export JAVA_HOME="${jdk}/lib/openjdk"
+          export GRAALVM_HOME="${graalvm}"
+          mkdir -p $HOME/jdks/jdk/${javaVersion} && ln -sfn "${jdk}/lib/openjdk" "$HOME/jdks/jdk/${javaVersion}"
+        '';
+      };
+    });
+}
